6229184bd8d09e5a43558ba5f1ab63f61e1b7a81
Brandon Feldkamp <brandon.feldkamp@gmail.com>
2022-12-18 14:35:14 -0500

    browns poo poo - from Kev

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ keywords = {
     "vfl": "Burn the VFL to the ground!",
     "armtalent": "Trevor Lawrence is the greatest quarterback of all time",
     "mikewhite": "Mike White. Mike White. Mike White. https://pbs.twimg.com/media/FimZOxHWYAAxShx.jpg",
+    "browns": "https://media.tenor.com/CmVlj5fLMXQAAAAC/utah-utes-tyler-huntley.gif",
     "jamaal": "He's a touchdown machine!!",
     "williams": "Williams? Jamaal Williams is a touchdown machine!!",
     "ridder": "My leaguemate Ben, he shows his Ridder to my other leaguemate Phil, and he says, \"You will never get this! You will never get this! La la la la la!\""
